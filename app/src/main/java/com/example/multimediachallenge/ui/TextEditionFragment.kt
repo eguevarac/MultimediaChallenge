7353d00fb8134a78e04aeec9b630f8dfa6171064
Quique <124347538+eguevarac@users.noreply.github.com>
2024-03-26 16:29:42 +0100

    working in textEdition

--- app/src/main/java/com/example/multimediachallenge/ui/TextEditionFragment.kt
+++ app/src/main/java/com/example/multimediachallenge/ui/TextEditionFragment.kt
@@ -1,6 +1,8 @@
 package com.example.multimediachallenge.ui
 
 import android.os.Bundle
+import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,6 +26,46 @@ class TextEditionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupSpinner()
+
+        binding.chbBold.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val originText = binding.etBody.text.toString().replace("\n", "<br>")
+                Log.i(">", "MUESTRA TEXT_ORIGIN")
+                Log.i(">", originText)
+                val boldText: String = "<b>" + originText + "</b>"
+                Log.i(">", "MUESTRA TEXT_BOLD")
+                Log.i(">", boldText)
+
+                /*boldText.replace("\n", "<br>")
+                Log.i(">", "MUESTRA TEXT_BOLD DESPUÉS DEL REPLACE")
+                Log.i(">", boldText)*/
+
+                val spanned = Html.fromHtml(boldText)
+                Log.i(">", "MUESTRA SPANNED")
+                Log.i(">", spanned.toString())
+                binding.etBody.setText(spanned)
+                /*if(binding.chbItalic.isChecked){
+
+                }else{
+
+                } */
+            } else {
+                val originText = binding.etBody.text.toString().replace("\n", "<br>")
+                val spanned = Html.fromHtml(originText)
+                //originText.replace("\n", "<br>")
+                binding.etBody.setText(spanned)
+                /*if(binding.chbItalic.isChecked){
+
+                }else{
+
+                }*/
+            }
+
+        }
+    }
+
+    private fun setupSpinner() {
         val spinnerOptions = listOf("8", "10", "12", "14", "16", "18", "20", "22")
         val adapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerOptions)
