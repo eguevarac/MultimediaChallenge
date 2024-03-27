59b3a6ed2f8592a0291bd4f5b19ea1126678b6d9
Quique <124347538+eguevarac@users.noreply.github.com>
2024-03-27 16:35:20 +0100

    working in textEdition

--- app/src/main/java/com/example/multimediachallenge/ui/TextEditionFragment.kt
+++ app/src/main/java/com/example/multimediachallenge/ui/TextEditionFragment.kt
@@ -2,10 +2,11 @@ package com.example.multimediachallenge.ui
 
 import android.os.Bundle
 import android.text.Html
-import android.util.Log
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.multimediachallenge.databinding.FragmentTextEditionBinding
@@ -28,45 +29,91 @@ class TextEditionFragment : Fragment() {
 
         setupSpinner()
 
-        binding.chbBold.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                val originText = binding.etBody.text.toString().replace("\n", "<br>")
-                Log.i(">", "MUESTRA TEXT_ORIGIN")
-                Log.i(">", originText)
-                val boldText: String = "<b>" + originText + "</b>"
-                Log.i(">", "MUESTRA TEXT_BOLD")
-                Log.i(">", boldText)
+        binding.chbBold.setOnCheckedChangeListener { _, _ -> setTextWithConditions() }
+        binding.chbItalic.setOnCheckedChangeListener { _, _ -> setTextWithConditions() }
 
-                /*boldText.replace("\n", "<br>")
-                Log.i(">", "MUESTRA TEXT_BOLD DESPUÉS DEL REPLACE")
-                Log.i(">", boldText)*/
-
-                val spanned = Html.fromHtml(boldText)
-                Log.i(">", "MUESTRA SPANNED")
-                Log.i(">", spanned.toString())
-                binding.etBody.setText(spanned)
-                /*if(binding.chbItalic.isChecked){
-
-                }else{
-
-                } */
-            } else {
-                val originText = binding.etBody.text.toString().replace("\n", "<br>")
-                val spanned = Html.fromHtml(originText)
-                //originText.replace("\n", "<br>")
-                binding.etBody.setText(spanned)
-                /*if(binding.chbItalic.isChecked){
-
-                }else{
-
-                }*/
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                setTextWithConditions()
             }
 
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                setTextWithConditions()
+            }
         }
     }
 
+    private fun setTextWithConditions() {
+        val originText = binding.etBody.text.toString().replace("\n", "<br>")
+        var spanned: Spanned? = null
+        var textToShow: String?
+
+        if (binding.chbBold.isChecked && !binding.chbItalic.isChecked) {
+
+            textToShow = "<b>" + originText + "</b>"
+            textToShow = setFontSize(textToShow)
+            spanned = Html.fromHtml(textToShow)
+
+        } else if (binding.chbBold.isChecked && binding.chbItalic.isChecked) {
+
+            textToShow = "<em><b>" + originText + "</b></em>"
+            textToShow = setFontSize(textToShow)
+            spanned = Html.fromHtml(textToShow)
+
+        } else if (!binding.chbBold.isChecked && binding.chbItalic.isChecked) {
+
+            textToShow = "<em>" + originText + "</em>"
+            textToShow = setFontSize(textToShow)
+            spanned = Html.fromHtml(textToShow)
+
+        } else if (!binding.chbBold.isChecked && !binding.chbItalic.isChecked) {
+
+            textToShow = setFontSize(originText)
+            spanned = Html.fromHtml(textToShow)
+        }
+
+        binding.etBody.setText(spanned)
+        //binding.etBody.textSize = binding.spinner.selectedItem.toString().toFloat()
+    }
+
+    private fun setFontSize(textToResize: String): String {
+        var textToShow: String?
+        when (binding.spinner.selectedItem) {
+            "h1" -> {
+                textToShow = "<h1>" + textToResize + "</h1>"
+            }
+
+            "h2" -> {
+                textToShow = "<h2>" + textToResize + "</h2>"
+            }
+
+            "h3" -> {
+                textToShow = "<h3>" + textToResize + "</h3>"
+            }
+
+            "h4" -> {
+                textToShow = "<h4>" + textToResize + "</h4>"
+            }
+
+            "h5" -> {
+                textToShow = "<h5>" + textToResize + "</h5>"
+            }
+
+            else -> {
+                textToShow = textToResize
+            }
+        }
+        return textToShow
+    }
+
+
     private fun setupSpinner() {
-        val spinnerOptions = listOf("8", "10", "12", "14", "16", "18", "20", "22")
+        val spinnerOptions = listOf("Tamaño normal", "h5", "h4", "h3", "h2", "h1")
         val adapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
