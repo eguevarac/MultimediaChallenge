0760a5765a95eb429d65652d3e28c667fe1095a9
Quique <124347538+eguevarac@users.noreply.github.com>
2024-03-29 20:37:39 +0100

    added video reproduction (working on it)

--- app/src/main/java/com/example/multimediachallenge/ui/VideoFragment.kt
+++ app/src/main/java/com/example/multimediachallenge/ui/VideoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.multimediachallenge.databinding.FragmentVideoBinding
 
@@ -11,11 +12,26 @@ import com.example.multimediachallenge.databinding.FragmentVideoBinding
 class VideoFragment : Fragment() {
 
     private lateinit var binding: FragmentVideoBinding
+    private val contractGallery =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            if (uri != null) {
+                binding.video.setVideoURI(uri)
+                binding.video.start()
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentVideoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnFindVideo.setOnClickListener {
+            contractGallery.launch("video/*")
+        }
     }
 }
