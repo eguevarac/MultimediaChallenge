974382c32239fbb5b83ddd49abca08918ea15441
Quique <124347538+eguevarac@users.noreply.github.com>
2024-03-29 19:43:57 +0100

    added contract to see img

--- app/src/main/java/com/example/multimediachallenge/ui/ImgFragment.kt
+++ app/src/main/java/com/example/multimediachallenge/ui/ImgFragment.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.multimediachallenge.databinding.FragmentImgBinding
 
 class ImgFragment : Fragment() {
 
     private lateinit var binding: FragmentImgBinding
+    private val contractGallery =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            if (uri != null) {
+                binding.img.setImageURI(uri)
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,4 +26,11 @@ class ImgFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnFindImg.setOnClickListener {
+            contractGallery.launch("image/*")
+        }
+    }
 }
